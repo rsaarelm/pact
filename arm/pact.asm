65de2b02cc4f90b9bb5f44c01d84fa78003e1577
Risto Saarelma <risto.saarelma@iki.fi>
2017-02-06 20:51:56 +0200

    Bare metal boot

--- arm/pact.asm
+++ arm/pact.asm
@@ -1,4 +1,34 @@
-    format binary
-@@:
+format binary
+use32
+thumb
+
+org     0x08000000
+
+RAM_START = 0x20000000
+RAM_LENGTH = 8192
+
+    dw RAM_START + RAM_LENGTH
+    dw reset + 1
+    dw error + 1
+    dw error + 1
+    dw error + 1
+    dw error + 1
+    dw error + 1
+    dw error + 1
+    dw error + 1
+    dw error + 1
+    dw error + 1
+    dw error + 1
+    dw error + 1
+    dw error + 1
+    dw error + 1
+    dw error + 1
+
+reset:
     nop
-    b @b
+    nop
+    b reset
+
+error:
+    nop
+    b error
