49a2dc4b36259b8c64b2ea49060aebd138f9a96f
Risto Saarelma <risto.saarelma@iki.fi>
2017-02-10 00:18:18 +0200

    Try to make it work on Cortex-M0

--- arm/pact.asm
+++ arm/pact.asm
@@ -36,13 +36,13 @@ GPIOA = 0x48000000
 ; r2
 ; r3
 ; r4
-; r5
-; r6
+; r5            Pact next instruction pointer   (NIP)
+; r6            Pact return stack pointer       (RSP)
 ; r7        avoid, ARM uses this for system calls
 ; r8
 ; r9        avoid, ARM may have special uses
-; r10           Pact next instruction pointer   (NIP)
-; r11           Pact return stack pointer       (RSP)
+; r10
+; r11
 ; r12       avoid, ARM uses thes for intra-procedure calls
 ; r13:  sp  ARM stack pointer
 ; r14:  lr  ARM link register, holds return address from Branch with Link
@@ -51,19 +51,22 @@ GPIOA = 0x48000000
 
 ; NEXT ends every machine code word by jumping to the next threaded position.
 macro NEXT {
-    ldr r0, [r10], #4   ; Load next instruction to R0, increment NIP
+    ldr r0, [r5]        ; Load next instruction to R0, increment NIP
+    adds r5, 4
     ldr r1, [r0]        ; Load next instruction address to R1
     bx  r1
 }
 
 ; Push to return stack
 macro PUSHRSP reg {
-    str reg, [r11, #-4]!    ; Store register to *(RSP - 4), decrement RSP
+    subs r6, 4
+    str reg, [r6]
 }
 
 ; Pop from return stack
 macro POPRSP reg {
-    ldr reg, [r11], #4      ; Load from RSP, increment RSP
+    ldr reg, [r6]          ; Load from RSP, increment RSP
+    adds r6, 4
 }
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Code
@@ -90,7 +93,7 @@ start:
     ; Init return stack
     ; XXX: Is there a nicer way to get the constant here than to use the
     ; intermediate mem addr?
-    ldr r11, [return_stack_end]
+    ldr r6, [return_stack_end]
     ; Init r0, as if we'd have called NEXT
     adr r0, pact_boot
     ; Jump to inner interpreter, using r0 value as the start word.
@@ -113,8 +116,8 @@ error:
 ;; Inner interpreter for threaded Pact bytecode
 ;; NEXT jumps here, so R0 will contain what we're running now.
 docol:
-    PUSHRSP r10     ; Store next instruction on return stack
-    add r10, r0, #4 ; Make NIP the next word from the code word.
+    PUSHRSP r5      ; Store next instruction on return stack
+    adds r5, r0, #4 ; Make NIP the next word from the code word.
     NEXT            ; Execute the word
 
 ; vim:syntax=fasm
